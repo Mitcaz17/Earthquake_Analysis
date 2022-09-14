24cda160c505b58d4d9f9a73cd3a623656674e81
Mitcaz17 <107505621+Mitcaz17@users.noreply.github.com>
2022-09-14 17:34:24 -0400

    Map

--- static/css/js/logic4.js
+++ static/css/js/logic4.js
@@ -41,7 +41,17 @@ let map = L.map('mapid', {
     layers: [streets]
 });
 
-L.control.layers(baseMaps).addTo(map);
+
+
+let earthquakes = new L.layerGroup();
+
+let overlays = {
+    Earthquakes: earthquakes
+};
+
+
+
+L.control.layers(baseMaps, overlays).addTo(map);
 
 // L.circleMarker([34.0522, -118.2437]).addTo(map);
 
@@ -152,7 +162,39 @@ d3.json(earthquakeData).then(function (data) {
         }
 
 
-    }).addTo(map);
+    }).addTo(earthquakes);
+
+    let legend = L.control({
+        position: "bottomright"
+    });
+
+
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+    
+
+        const magnitudes = [0, 1, 2, 3, 4, 5];
+        const colors = [
+            "#98ee00",
+            "#d4ee00",
+            "#eecc00",
+            "#ee9c00",
+            "#ea822c",
+            "#ea2c2c"
+        ];
+
+        for (var i = 0; i < magnitudes.length; i++) {
+            console.log(colors[i]);
+            div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
+   
+    legend.addTo(map);
+
+
 
 
 
